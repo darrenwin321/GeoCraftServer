69bd8c4bfeb7e8b92ec5eeda768e04c8adb15bb9
darrenwin321 <89400271+darrenwin321@users.noreply.github.com>
2023-11-18 04:44:26 -0500

    Revert "Connected backend to our MongoDB"
    This reverts commit 809796f3178cf17db7d229f832e7ea8271250da1.

--- models/user-model.js
+++ models/user-model.js
@@ -6,11 +6,11 @@ const UserSchema = new Schema(
     {
         firstName: { type: String, required: true },
         lastName: { type: String, required: true },
-        username: { type: String, required: true },
         email: { type: String, required: true },
-        passwordHash: { type: String, required: true }
+        username: { type: String, required: true},
+        passwordHash: { type: String, required: true },
     },
     { timestamps: true },
 )
-
+  
 module.exports = mongoose.model('User', UserSchema)
